8ae0de2b469d554ac637ca20f1bfa1ea13a8a2a2
WaterCalm <helory@qq.com>
2020-10-17 00:07:47 +0800

    add baidu tongji

---
+++ node_modules/gitbook-plugin-baidu-tongji-with-multiple-channel/index.js
@@ -0,0 +1,17 @@
+/*
+ *  ===============================================================
+ *      snowdreams1006 is not just for programmers
+ *  ===============================================================
+ *
+ *  - Document: index.js
+ *  - Author: snowdreams1006
+ *  - Description: Gitbook plugin index
+ */
+module.exports = {
+    book: {
+        assets: "./assets",
+        js: [
+            "plugin.js"
+        ]
+    }
+};
